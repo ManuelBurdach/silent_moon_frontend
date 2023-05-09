968d6e6a155642b798911727eddb1cf4d7ae7b9b
ManuelBurdach <103679985+ManuelBurdach@users.noreply.github.com>
2023-05-09 16:09:30 +0200

    update login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,71 +3,82 @@ import { useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import BackButton from "../../components/BackButton/BackButton";
+import { userState } from "../../state/userState";
 
 const Login = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const userLoginRef = useRef();
-    const passwordLoginRef = useRef();
+  const userLoginRef = useRef();
+  const passwordLoginRef = useRef();
 
-    const login = async (event) => {
-        event.preventDefault();
+  const setUser = userState((state) => state.setUser);
 
-        const userLogin = userLoginRef.current.value;
-        const passwordLogin = passwordLoginRef.current.value;
+  const login = async (event) => {
+    event.preventDefault();
 
-        try {
-            const result = await fetch("http://localhost:4545/login", {
-                method: "POST",
-                credentials: "include",
-                headers: {
-                    "content-type": "application/json",
-                },
-                body: JSON.stringify({
-                    user: userLogin,
-                    password: passwordLogin,
-                }),
-            });
+    const userLogin = userLoginRef.current.value;
+    const passwordLogin = passwordLoginRef.current.value;
 
-            if (result.ok) {
-                console.log("Logged in user: " + userLogin);
-                navigate("/");
-            } else {
-                console.log("Error: ", result.statusText);
-            }
-        } catch (error) {
-            console.log("Error: ", error);
+    try {
+      const result = await fetch(
+        import.meta.env.VITE_BACKEND + import.meta.env.VITE_API_VERSION + "/user/login",
+        {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            email: userLogin,
+            password: passwordLogin,
+          }),
         }
-    };
+      );
 
-    return (
-        <section className="loginAndRegister">
-            <BackButton />
-            <h1>Welcome Back!</h1>
-            <form onSubmit={login}>
-                <input
-                    ref={userLoginRef}
-                    type="text"
-                    name="userLogin"
-                    id="userLogin"
-                    placeholder="EMAIL"
-                    required
-                />
-                <input
-                    ref={passwordLoginRef}
-                    type="password"
-                    name="passwordLogin"
-                    id="passwordLogin"
-                    placeholder="PASSWORD"
-                    required
-                />
-                <input type="submit" value="LOGIN" className="bigRedButton" />
-            </form>
-            <p>
-                Don't have an account yet? <Link to="/register">Sign up</Link>
-            </p>
-        </section>
-    );
+      const response = await result.json();
+      if (result.ok) {
+        console.log("Login korrekt");
+        setUser(response);
+        navigate("/welcome"); //auf home weiterleiten
+      } else if (result.status === 401) {
+        //Hinweis ins frontend
+        console.log("Error: Userlogindaten fehlerhaft");
+      } else {
+        console.log("Error: Server error");
+      }
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  return (
+    <section className="loginAndRegister">
+      <BackButton />
+      <h1>Welcome Back!</h1>
+      <form onSubmit={login}>
+        <input
+          ref={userLoginRef}
+          type="email"
+          name="userLogin"
+          id="userLogin"
+          placeholder="EMAIL"
+          required
+        />
+        <input
+          ref={passwordLoginRef}
+          type="password"
+          name="passwordLogin"
+          id="passwordLogin"
+          placeholder="PASSWORD"
+          required
+        />
+        <input type="submit" value="LOGIN" className="bigRedButton" />
+      </form>
+      <p>
+        Don't have an account yet? <Link to="/register">Sign up</Link>
+      </p>
+    </section>
+  );
 };
 
 export default Login;
