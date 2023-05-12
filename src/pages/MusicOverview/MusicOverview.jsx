3c6df21b7dd9a221c1224a6f56ecda651b6485ac
veronicamayer <mayerveronica.vm@gmail.com>
2023-05-12 15:44:03 +0200

    insert .env client id and secret in music page

--- src/pages/MusicOverview/MusicOverview.jsx
+++ src/pages/MusicOverview/MusicOverview.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import "./MusicOverview.scss";
 import Navigation from "../../components/Navigation/Navigation";
 
-const CLIENT_ID = "";
-const CLIENT_SECRET = "";
+const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+const CLIENT_SECRET = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
 const PLAYLIST_ID = "37i9dQZF1DWYBO1MoTDhZI";
 
 const MusicOverview = () => {
