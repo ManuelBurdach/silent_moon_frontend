af81485b04666fa53e9eaa2a28022433daddd54f
veronicamayer <mayerveronica.vm@gmail.com>
2023-05-17 01:29:14 +0200

    add + delete Fav & new css reminder time picker

---
+++ src/pages/UserProfile/UserProfile.jsx
@@ -0,0 +1,189 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import DefaultResult from "../../assets/images/DefaultResult.png";
+import "./UserProfile.scss";
+import { userState } from "../../state/userState";
+import Navigation from "../../components/Navigation/Navigation";
+
+const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+const CLIENT_SECRET = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
+
+const UserProfile = () => {
+    const [favoritePlaylists, setFavoritePlaylists] = useState([]);
+    const [favoriteVideos, setFavoriteVideos] = useState([]);
+    const [accessToken, setAccessToken] = useState("");
+    const [playlists, setPlaylists] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const user = userState((state) => state.user);
+
+    useEffect(() => {
+        const fetchUserDetails = async () => {
+            try {
+                const response = await fetch(
+                    import.meta.env.VITE_BACKEND +
+                        import.meta.env.VITE_API_VERSION +
+                        "/user/details",
+                    { credentials: "include" }
+                );
+                if (response.ok) {
+                    const data = await response.json();
+                    setFavoritePlaylists(data.favoriteMeditation);
+                    setFavoriteVideos(data.favoriteYoga);
+                } else {
+                    const result = response.json();
+                    throw new Error("Error getting user details");
+                }
+            } catch (error) {
+                console.error("Error getting user details: ", error);
+            }
+        };
+        fetchUserDetails();
+    }, []);
+
+    useEffect(() => {
+        // API Access Token
+        var authParameters = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body:
+                "grant_type=client_credentials&client_id=" +
+                CLIENT_ID +
+                "&client_secret=" +
+                CLIENT_SECRET,
+        };
+
+        fetch("https://accounts.spotify.com/api/token", authParameters)
+            .then((result) => result.json())
+            .then((data) => {
+                setAccessToken(data.access_token);
+                fetchPlaylists(data.access_token);
+            });
+    }, []);
+
+    function fetchPlaylists(accessToken) {
+        const headers = {
+            Authorization: `Bearer ${accessToken}`,
+        };
+
+        const promises = favoritePlaylists.map((playlistId) =>
+            fetch(`https://api.spotify.com/v1/playlists/${playlistId}`, {
+                headers: headers,
+            }).then((result) => result.json())
+        );
+
+        Promise.all(promises)
+            .then((data) => {
+                setPlaylists(data);
+            })
+            .catch((error) => {
+                console.error("Error fetching playlists:", error);
+            });
+    }
+
+    console.log(favoritePlaylists);
+    console.log(favoriteVideos);
+
+    return (
+        <section id="userProfile">
+            <h4 className="logo">SILENT MOON</h4>
+
+            <h1 className="heading1">Hi {user.firstName}!</h1>
+
+            <form>
+                <input
+                    type="text"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+            </form>
+            <article className="homeVideos">
+                <h2 className="heading2">Your Favorite Yoga Sessions</h2>
+                <div>
+                    {favoriteVideos
+                        .filter((video) =>
+                            video.title
+                                .toLowerCase()
+                                .includes(searchQuery.toLowerCase())
+                        )
+                        .map((video) => (
+                            <Link
+                                to={`/yogadetails/${video._id}`}
+                                key={video._id}
+                            >
+                                <video controls={false}>
+                                    <source src={video.url} />
+                                </video>
+
+                                <h3 className="heading2">{video.title}</h3>
+                                <div>
+                                    <p className="textSmall uppercase">
+                                        {video.level}
+                                    </p>
+                                    <p className="textSmall uppercase">
+                                        {video.duration} min
+                                    </p>
+                                </div>
+                            </Link>
+                        ))}
+                    {favoriteVideos.filter((video) =>
+                        video.title
+                            .toLowerCase()
+                            .includes(searchQuery.toLowerCase())
+                    ).length === 0 && (
+                        <Link>
+                            <img src={DefaultResult} alt="" />
+
+                            <h3 className="heading2">No yoga video found</h3>
+                        </Link>
+                    )}
+                </div>
+            </article>
+            <article className="homeVideos">
+                <h2 className="heading2">Your Favorite Meditations</h2>
+                <div>
+                    {playlists
+                        .filter((playlist) =>
+                            playlist.name
+                                .toLowerCase()
+                                .includes(searchQuery.toLowerCase())
+                        )
+                        .map((playlist) => (
+                            <Link
+                                to={`/meditatedetails/${playlist.id}`}
+                                key={playlist.id}
+                            >
+                                <img src={playlist.images[0].url} alt="" />
+
+                                <h3 className="heading2">{playlist.name}</h3>
+                                <div>
+                                    <p className="textSmall uppercase">
+                                        playlist
+                                    </p>
+                                    <p className="textSmall uppercase">
+                                        {playlist.tracks.total} items
+                                    </p>
+                                </div>
+                            </Link>
+                        ))}
+                    {playlists.filter((playlist) =>
+                        playlist.name
+                            .toLowerCase()
+                            .includes(searchQuery.toLowerCase())
+                    ).length === 0 && (
+                        <Link>
+                            <img src={DefaultResult} alt="" />
+
+                            <h3 className="heading2">No meditation found</h3>
+                        </Link>
+                    )}
+                </div>
+            </article>
+            <Navigation />
+        </section>
+    );
+};
+
+export default UserProfile;
--- src/pages/YogaDetails/YogaDetails.jsx
+++ src/pages/YogaDetails/YogaDetails.jsx
@@ -9,111 +9,133 @@ import LikeButton from "../../components/LikeButton/LikeButton";
 import { userState } from "../../state/userState";
 
 const YogaDetails = () => {
-  const [selectedVideo, setSelectedVideo] = useState([]);
-  let { videoId } = useParams();
-  const nav = useNavigate();
-  const setUser = userState((state) => state.setUser);
+    const [selectedVideo, setSelectedVideo] = useState([]);
+    let { videoId } = useParams();
+    const nav = useNavigate();
+    const setUser = userState((state) => state.setUser);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          import.meta.env.VITE_BACKEND +
-            import.meta.env.VITE_API_VERSION +
-            "/data/videos/" +
-            videoId,
-          { credentials: "include" }
-        );
-        if (response.ok) {
-          const data = await response.json();
-          setSelectedVideo(data);
-        } else {
-          const result = response.json();
-          setUser(result);
-          nav("/login");
-          throw new Error("Error fetching video");
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(
+                    import.meta.env.VITE_BACKEND +
+                        import.meta.env.VITE_API_VERSION +
+                        "/data/videos/" +
+                        videoId,
+                    { credentials: "include" }
+                );
+                if (response.ok) {
+                    const data = await response.json();
+                    setSelectedVideo(data);
+                } else {
+                    const result = response.json();
+                    setUser(result);
+                    nav("/login");
+                    throw new Error("Error fetching video");
+                }
+            } catch (error) {
+                console.error("Error fetching video:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const videoRef = useRef(null);
+    const buttonRef = useRef(null);
+    const [isButtonVisible, setIsButtonVisible] = useState(true);
+    const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+
+    const handlePlayClick = () => {
+        if (videoRef.current) {
+            if (videoRef.current.paused) {
+                videoRef.current.play();
+                setIsButtonVisible(false);
+                setIsVideoPlaying(true);
+            } else {
+                videoRef.current.pause();
+                setIsVideoPlaying(false);
+            }
         }
-      } catch (error) {
-        console.error("Error fetching video:", error);
-      }
     };
 
-    fetchData();
-  }, []);
+    const handleVideoClick = (event) => {
+        if (videoRef.current && !videoRef.current.paused) {
+            setIsButtonVisible(true);
+        }
+    };
 
-  const videoRef = useRef(null);
-  const buttonRef = useRef(null);
-  const [isButtonVisible, setIsButtonVisible] = useState(true);
-  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+    const addFavorite = async (video) => {
+        try {
+            const response = await fetch(
+                import.meta.env.VITE_BACKEND +
+                    import.meta.env.VITE_API_VERSION +
+                    "/user/addYogaFav",
+                {
+                    method: "POST",
+                    credentials: "include",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ favorite: video }),
+                }
+            );
+            if (response.ok) {
+                const data = await response.json();
+                console.log("favorite saved");
+            } else {
+                const result = response.json();
+                throw new Error("Error saving favorite: " + result);
+            }
+        } catch (error) {
+            console.error("Error saving favorite:", error);
+        }
+    };
 
-  const handlePlayClick = () => {
-    if (videoRef.current) {
-      if (videoRef.current.paused) {
-        videoRef.current.play();
-        setIsButtonVisible(false);
-        setIsVideoPlaying(true);
-      } else {
-        videoRef.current.pause();
-        setIsVideoPlaying(false);
-      }
-    }
-  };
+    return (
+        <section id="yogaDetails">
+            <BackButton addClass="fill" />
+            <button
+                className="likeButton"
+                onClick={() => addFavorite(selectedVideo)}
+            ></button>
+            <article className="yogaVideo">
+                {selectedVideo.url && (
+                    <video
+                        controls={false}
+                        ref={videoRef}
+                        onClick={handleVideoClick}
+                    >
+                        <source src={selectedVideo.url} />
+                    </video>
+                )}
 
-  const handleVideoClick = (event) => {
-    if (videoRef.current && !videoRef.current.paused) {
-      setIsButtonVisible(true);
-    }
-  };
-
-  const downloadVideo = () => {
-    var videoUrl =
-      "https://player.vimeo.com/external/543260662.sd.mp4?s=350439a672fd906513eda962d3386a5b01f3ba0c&profile_id=164"; // Replace with the actual video URL
-    var a = document.createElement("a");
-    a.href = videoUrl;
-    a.download = "video.mp4"; // Specify the filename and extension for the downloaded video
-    a.style.display = "none";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-  };
-
-  return (
-    <section id="yogaDetails">
-      <BackButton addClass="fill" />
-      <LikeButton />
-      <article className="yogaVideo">
-        {selectedVideo.url && (
-          <video controls={false} ref={videoRef} onClick={handleVideoClick}>
-            <source src={selectedVideo.url} />
-          </video>
-        )}
-
-        {isButtonVisible && (
-          <button ref={buttonRef} onClick={handlePlayClick}>
-            {isVideoPlaying ? (
-              <img src={PauseButton} alt="pause" />
-            ) : (
-              <img src={PlayButton} alt="play" />
-            )}
-          </button>
-        )}
-      </article>
-      <article className="yogaDetails">
-        <h1 className="heading1">{selectedVideo.title}</h1>
-        <p className="textSmall uppercase">{selectedVideo.level}</p>
-        <p className="textSmall">{selectedVideo.description}</p>
-        <div>
-          <p className="textSmall yogaFavoritesAndViews yogaFavorites">
-            {(selectedVideo.favorites / 1000).toFixed(3)} Favorites
-          </p>
-          <p className="textSmall yogaFavoritesAndViews yogaViews">
-            {(selectedVideo.views / 1000).toFixed(3)} Views
-          </p>
-        </div>
-      </article>
-      <Navigation />
-    </section>
-  );
+                {isButtonVisible && (
+                    <button ref={buttonRef} onClick={handlePlayClick}>
+                        {isVideoPlaying ? (
+                            <img src={PauseButton} alt="pause" />
+                        ) : (
+                            <img src={PlayButton} alt="play" />
+                        )}
+                    </button>
+                )}
+            </article>
+            <article className="yogaDetails">
+                <h1 className="heading1">{selectedVideo.title}</h1>
+                <p className="textSmall uppercase">{selectedVideo.level}</p>
+                <p className="textSmall">{selectedVideo.description}</p>
+                <div>
+                    <p className="textSmall yogaFavoritesAndViews yogaFavorites">
+                        {(selectedVideo.favorites / 1000).toFixed(3)} Favorites
+                    </p>
+                    <p className="textSmall yogaFavoritesAndViews yogaViews">
+                        {(selectedVideo.views / 1000).toFixed(3)} Views
+                    </p>
+                </div>
+            </article>
+            <Navigation />
+        </section>
+    );
 };
 
 export default YogaDetails;
